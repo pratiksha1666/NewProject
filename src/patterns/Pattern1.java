07691416085e0bfabb8b878dd19668ecaccacaec
Pratiksha <pratikshagade20@gmail.com>
2023-02-11 18:36:50 +0530

    Java

--- src/patterns/Pattern1.java
+++ src/patterns/Pattern1.java
@@ -1,4 +1,13 @@
 package patterns;
 
 public class Pattern1 {
+    public static void main(String[] args) {
+        for(int i=1;i<=4;i++)
+        {
+            for(int j=1;j<=i;j++) {
+                System.out.println("*");
+            }
+          //g  System.out.print();
+        }
+    }
 }
